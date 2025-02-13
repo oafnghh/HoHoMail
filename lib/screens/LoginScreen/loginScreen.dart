bd1c07388f0bfe1bcb2fa9190fb3e71d95741315
oafnghh <minhhoanglee11@gmail.com>
2025-02-13 23:30:07 +0700

    Dev_Hoang_250213_UiLogin
    UiLogin

---
+++ lib/screens/LoginScreen/loginScreen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var orange = Colors.orange;
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body:Container(
+        padding: EdgeInsets.symmetric(vertical: 30),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            colors: [
+              orange[900]!,
+              orange[800]!,
+              orange[400]!
+          ])
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child:Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("Login",style: TextStyle(color: Colors.white,fontSize: 40),),
+                  SizedBox(height: 10,),
+                  Text("Welcome Back",style: TextStyle(color: Colors.white,fontSize: 18),)
+                ]
+              ) ,
+            ),
+            SizedBox(height: 20,),
+            Expanded(
+              child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child:SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 60,),
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromRGBO(255, 95, 27, .3),
+                              blurRadius: 20,
+                              offset: Offset(0, 10)
+                            )
+                          ]
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide(color: Colors.grey[200]!))
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: "Email",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10,),
+                            Container(
+                              decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide(color: Colors.grey[200]!))
+                              ),
+                              child: TextField(
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(color: Colors.grey),
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 40,),
+                      SizedBox(child: MaterialButton(
+                        onPressed: (){},
+                        height: 50,
+                        color: Colors.orange[900]!,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Center(
+                            child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                          ),
+                      ))
+                    ],
+                  ),
+                ),
+              ),
+            ))
+          ]
+        ),
+      ),
+    );
+  }
+}
