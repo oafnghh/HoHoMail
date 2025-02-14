2e86820bd84c84b0bc6ab9e4c7e5d730f9037a2f
oafnghh <minhhoanglee11@gmail.com>
2025-02-14 08:34:29 +0700

    Dev_Hoang_250214
    Register, validator

--- lib/screens/RegisterScreen/registerScreen.dart
+++ lib/screens/RegisterScreen/registerScreen.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:hoho_mail/screens/RegisterScreen/registerScreen.dart';
+import 'package:hoho_mail/screens/LoginScreen/loginScreen.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<RegisterScreen> createState() => _RegisterScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
-    final formKey = GlobalKey<FormState>();
     var orange = Colors.orange;
+    final formKey = GlobalKey<FormState>();
     return Scaffold(
       resizeToAvoidBottomInset: true,
       body:Container(
@@ -52,24 +52,24 @@ class _LoginScreenState extends State<LoginScreen> {
               child: Padding(
                 padding: EdgeInsets.all(20),
                 child:SingleChildScrollView(
-                  child: Form(
-                    key: formKey,
-                    child: Column(
-                      children: <Widget>[
-                        SizedBox(height: 60,),
-                        Container(
-                          padding: EdgeInsets.all(20),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Color.fromRGBO(255, 95, 27, .3),
-                                blurRadius: 20,
-                                offset: Offset(0, 10)
-                              )
-                            ]
-                          ),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 60,),
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromRGBO(255, 95, 27, .3),
+                              blurRadius: 20,
+                              offset: Offset(0, 10)
+                            )
+                          ]
+                        ),
+                        child: Form(
+                        key: formKey,
                           child: Column(
                             children: <Widget>[
                               Container(
@@ -109,42 +109,66 @@ class _LoginScreenState extends State<LoginScreen> {
                                     border: InputBorder.none,
                                   ),
                                 ),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Colors.grey[200]!))
+                                ),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    hintText: "What is your name ?",
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color: Colors.grey[200]!))
+                                ),
+                                child: TextFormField(
+                                  decoration: InputDecoration(
+                                    hintText: "What is your number phone ?",
+                                    hintStyle: TextStyle(color: Colors.grey),
+                                    border: InputBorder.none
+                                  ),
+                                ),
                               )
                             ],
                           ),
                         ),
-                        SizedBox(height: 40,),
-                        SizedBox(child: MaterialButton(
-                          onPressed: (){
-                            if(formKey.currentState!.validate()){
+                      ),
+                      SizedBox(height: 40,),
+                      SizedBox(child: MaterialButton(
+                        onPressed: (){
+                          if(formKey.currentState!.validate()){
                               ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(content: Text('hihih')),
+                                const SnackBar(content: Text('Success')),
                               );
-                            }
-                          },
-                          height: 50,
-                          color: Colors.orange[900]!,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50),
-                          ),
-                          child: Center(
-                            child: Text("Login", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                          ),
-                        )),
-                        SizedBox(height: 20,),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children:<Widget>[
-                            Text("Tôi chưa có tài khoản ?",),
-                            TextButton(onPressed:(){
-                            Navigator.of(context).push(
-                              MaterialPageRoute(builder: (context)=> const RegisterScreen()),
-                            );
-                            } , child: Text("Đăng kí", style: TextStyle(color: const Color.fromARGB(255, 91, 62, 223), fontWeight: FontWeight.bold),)),
-                          ]
-                        )
-                      ],
-                    ),
+                          }
+                        },
+                        height: 50,
+                        color: Colors.orange[900]!,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Center(
+                            child: Text("Register", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                        ),
+                      )),
+                      SizedBox(height: 20,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children:<Widget>[
+                          Text("Tôi đã có tài khoản ?",),
+                          TextButton(onPressed:(){
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context)=> const LoginScreen()),
+                          );
+                          } , child: Text("Đăng Nhập", style: TextStyle(color: const Color.fromARGB(255, 91, 62, 223), fontWeight: FontWeight.bold),)),
+                        ]
+                      )
+                    ],
                   ),
                 ),
               ),
