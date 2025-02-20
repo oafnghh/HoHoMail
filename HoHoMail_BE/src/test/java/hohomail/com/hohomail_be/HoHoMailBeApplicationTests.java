98e71f8a1529d57c3e0c0b8df2b57407d75f20c8
ThuongHoai081 <th316942@gmail.com>
2025-02-20 14:51:34 +0700

    init

---
+++ HoHoMail_BE/src/test/java/hohomail/com/hohomail_be/HoHoMailBeApplicationTests.java
@@ -0,0 +1,13 @@
+package hohomail.com.hohomail_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoHoMailBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
